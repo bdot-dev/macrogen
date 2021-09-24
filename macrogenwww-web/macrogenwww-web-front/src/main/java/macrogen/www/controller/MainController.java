473287b889d3e3df818f7e3c7fe7b970c98b16b1
kimjd72 <kimjd72@gmail.com>
2021-09-24 20:23:53 +0900

    misc

--- macrogenwww-web/macrogenwww-web-front/src/main/java/macrogen/www/controller/MainController.java
+++ macrogenwww-web/macrogenwww-web-front/src/main/java/macrogen/www/controller/MainController.java
@@ -3,7 +3,6 @@ package macrogen.www.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import macrogen.www.enums.LangId;
 import macrogen.www.service.NttService;
-import macrogen.www.vo.MngrVo;
 import macrogen.www.vo.NttVo;
 
 @Controller
