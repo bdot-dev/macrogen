e8111326e92609a2e598954cb5051987d02e09be
kimjd72 <kimjd72@gmail.com>
2021-12-21 13:35:03 +0900

    모바일 - 미디어라이브러리 - 이미지상세 보기 오류 수정

--- macrogenwww-web/macrogenwww-web-front/src/main/java/macrogen/www/controller/AppController.java
+++ macrogenwww-web/macrogenwww-web-front/src/main/java/macrogen/www/controller/AppController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * <pre>
  * macrogen.www.controller
  *    |_ AppController.java
- * 개요 :
+ * 개요 : 모바일기기용 임시페이지
  * </pre>
  * 1. 작성일 : 2021. 9. 14. 오후 5:56:33
  * 2. 작성자 : eluocnc
@@ -19,6 +19,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/app")
 public class AppController extends DefaultController {
 
+	/**
+	 * <pre>
+	 * main
+	 * 1. 개요   : 모바일기기용 임시페이지
+	 * 2. 작성자 : eluocnc
+	 * </pre>
+	 *
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
 	@RequestMapping(method = RequestMethod.GET)
 	public String main(Model model) throws Exception {
 
